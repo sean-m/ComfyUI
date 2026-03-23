c5b825c5a55d9b75367b7e23e02e9ef008e89906
Sean McArdle <sean.mcardle0@gmail.com>
2026-03-23 13:51:56 -0700

    Remove comfy_aimdo.control initialization
    Remove import and initialization of comfy_aimdo.control as it's CUDA only and I'm not that cool

--- main.py
+++ main.py
@@ -29,11 +29,6 @@ setup_logger(log_level=args.verbose, use_stdout=args.log_stdout)
 
 faulthandler.enable(file=sys.stderr, all_threads=False)
 
-import comfy_aimdo.control
-
-if enables_dynamic_vram():
-    comfy_aimdo.control.init()
-
 if os.name == "nt":
     os.environ['MIMALLOC_PURGE_DELAY'] = '0'
 
